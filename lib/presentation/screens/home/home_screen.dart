a68350ba1011fe17f1062dbdbdc0b622abf4d63b
Alex S <alexs1010@gmail.com>
2024-03-11 11:16:51 -0600

    add list view with menu

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,14 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_widgets_practice/config/menu/menu_item.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
-
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Wigdets miscellaneous'),        
       ),
+      body: const _HomeView(),
+    );
+  }
+}
+
+class _HomeView extends StatelessWidget{
+  const _HomeView();
+
+  @override
+  Widget build(BuildContext context){
+    return ListView.builder(
+      itemCount: appMenuItems.length,
+      itemBuilder: (context, int index) {
+        final menuItem = appMenuItems[index];
+        return _CustomListTile(menuItem: menuItem);        
+      }
+    );
+  }
+}
+
+class _CustomListTile extends StatelessWidget {
+  const _CustomListTile({
+    required this.menuItem,
+  });
+
+  final MenuItem menuItem;
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return ListTile(
+      leading: Icon(menuItem.icon, color: colors.primary,),
+      trailing: Icon(Icons.arrow_forward_ios_rounded, color: colors.primary,),
+      title: Text(menuItem.title),
+      subtitle: Text(menuItem.subTitle),
+      onTap: () {
+        //Todo navigator screen
+      },
     );
   }
 }
